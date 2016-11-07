c8eb1ff55a4726d84c46e01f784e3b3be95dfe8b
ysqi <devysq@gmail.com>
2016-11-07 21:03:39 +0800

    mini css

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,15 +5,16 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-gh-pages');
     grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    grunt.loadNpmTasks('grunt-contrib-cssmin'); 
     grunt.loadNpmTasks('grunt-exec');
-    
-    var SOURCE_DIR='public'
+
+    var SOURCE_DIR = 'public'
 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         clean: {
             tmp: ['tmp/**/*'],
-            deploy: [SOURCE_DIR+'/**/*']
+            deploy: [SOURCE_DIR + '/**/*']
         },
         exec: {
             buildContent: {
@@ -25,25 +26,39 @@ module.exports = function (grunt) {
                 options: {                                  // Target options
                     removeComments: true,
                     collapseWhitespace: true,
-                    minifyCSS: true, 
+                    minifyCSS: true,
                 },
-                files:[ 
-                {
-                    expand: true,     // Enable dynamic expansion.
-                    cwd: SOURCE_DIR,      // Src matches are relative to this path.
-                    src: ['**/*.html'], // Actual pattern(s) to match.
-                    dest: SOURCE_DIR,   // Destination path prefix.
-                }], 
-        }
-    },
+                files: [
+                    {
+                        expand: true,     // Enable dynamic expansion.
+                        cwd: SOURCE_DIR,      // Src matches are relative to this path.
+                        src: ['**/*.html'], // Actual pattern(s) to match.
+                        dest: SOURCE_DIR,   // Destination path prefix.
+                    }],
+            }
+        },
+        cssmin: {
+            options: {
+                keepSpecialComments: 0, 
+            },
+            target: {
+                 files: [
+                    {
+                        expand: true,     // Enable dynamic expansion.
+                        cwd: SOURCE_DIR,      // Src matches are relative to this path.
+                        src: ['**/*.css'], // Actual pattern(s) to match.
+                        dest: SOURCE_DIR,   // Destination path prefix.
+                    }],
+            }
+        },
         'gh-pages': {
             options: {
                 branch: process.env.DEPLOY_BRANCH,
-                base:  SOURCE_DIR, 
+                base: SOURCE_DIR,
             },
             publish: {
                 options: {
-                    repo:  'https://'+process.env.REPO,
+                    repo: 'https://' + process.env.REPO,
                     message: 'publish gh-pages (cli)'
                 },
                 src: ['**/*']
@@ -51,11 +66,11 @@ module.exports = function (grunt) {
             deploy: {
                 options: {
                     user: {
-                        name:  process.env.USER ,
+                        name: process.env.USER,
                         email: process.env.EMAIL,
                     },
-                    repo: 'https://' + process.env.GH_TOKEN + '@'+process.env.REPO,
-                    message: '[ci skip] deploy from '+process.env.USER+' by travis' + getDeployMessage(),
+                    repo: 'https://' + process.env.GH_TOKEN + '@' + process.env.REPO,
+                    message: '[ci skip] deploy from ' + process.env.USER + ' by travis' + getDeployMessage(),
                     //With silent true log messages are suppressed and error messages are sanitized.
                     silent: true
                 },
@@ -64,59 +79,60 @@ module.exports = function (grunt) {
         }
     });
 
-// get a formatted commit message to review changes from the commit log
-// github will turn some of these into clickable links
-function getDeployMessage() {
-    var ret = '\n\n';
-    if (process.env.TRAVIS !== 'true') {
-        ret += 'missing env vars for travis-ci';
+    // get a formatted commit message to review changes from the commit log
+    // github will turn some of these into clickable links
+    function getDeployMessage() {
+        var ret = '\n\n';
+        if (process.env.TRAVIS !== 'true') {
+            ret += 'missing env vars for travis-ci';
+            return ret;
+        }
+        ret += 'branch:       ' + process.env.TRAVIS_BRANCH + '\n';
+        ret += 'SHA:          ' + process.env.TRAVIS_COMMIT + '\n';
+        ret += 'range SHA:    ' + process.env.TRAVIS_COMMIT_RANGE + '\n';
+        ret += 'build id:     ' + process.env.TRAVIS_BUILD_ID + '\n';
+        ret += 'build number: ' + process.env.TRAVIS_BUILD_NUMBER + '\n';
         return ret;
     }
-    ret += 'branch:       ' + process.env.TRAVIS_BRANCH + '\n';
-    ret += 'SHA:          ' + process.env.TRAVIS_COMMIT + '\n';
-    ret += 'range SHA:    ' + process.env.TRAVIS_COMMIT_RANGE + '\n';
-    ret += 'build id:     ' + process.env.TRAVIS_BUILD_ID + '\n';
-    ret += 'build number: ' + process.env.TRAVIS_BUILD_NUMBER + '\n';
-    return ret;
-}
 
-grunt.registerTask('check-deploy', function () {
-    // need this
-    this.requires(['build']);
+    grunt.registerTask('check-deploy', function () {
+        // need this
+        this.requires(['build']);
 
-    // only deploy under these conditions
-    if (process.env.TRAVIS === 'true' && process.env.TRAVIS_SECURE_ENV_VARS === 'true' && process.env.TRAVIS_PULL_REQUEST === 'false') {
-        grunt.log.writeln('executing deployment');
-        // queue deploy
-        grunt.task.run('gh-pages:deploy');
-    }
-    else {
-        grunt.log.writeln('skipped deployment');
-    }
-});
+        // only deploy under these conditions
+        if (process.env.TRAVIS === 'true' && process.env.TRAVIS_SECURE_ENV_VARS === 'true' && process.env.TRAVIS_PULL_REQUEST === 'false') {
+            grunt.log.writeln('executing deployment');
+            // queue deploy
+            grunt.task.run('gh-pages:deploy');
+        }
+        else {
+            grunt.log.writeln('skipped deployment');
+        }
+    });
 
-grunt.registerTask('prep', 'Clean-up project', [
-    'clean',
-]);
+    grunt.registerTask('prep', 'Clean-up project', [
+        'clean',
+    ]);
 
-grunt.registerTask('build', 'Rebuild locally', [
-    'prep',
-    "exec:buildContent",
-    'htmlmin:dist'
-]);
+    grunt.registerTask('build', 'Rebuild locally', [
+        'prep',
+        "exec:buildContent",
+        'htmlmin:dist',
+        'cssmin'
+    ]);
 
-grunt.registerTask('publish', 'Publish from CLI', [
-    'build',
-    'gh-pages:publish'
-]);
+    grunt.registerTask('publish', 'Publish from CLI', [
+        'build',
+        'gh-pages:publish'
+    ]);
 
-grunt.registerTask('deploy', 'Publish from Travis', [
-    'build',
-    'check-deploy'
-]);
+    grunt.registerTask('deploy', 'Publish from Travis', [
+        'build',
+        'check-deploy'
+    ]);
 
-// per convention set a test task
-grunt.registerTask('test', ['build']);
+    // per convention set a test task
+    grunt.registerTask('test', ['build']);
 
-grunt.registerTask('default', ['test']);
+    grunt.registerTask('default', ['test']);
 };
